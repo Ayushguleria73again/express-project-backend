b01bb1e1fa31253e4d00f29e44d8ab8b719e1126
Ayushguleria73again <ayushguleria73@gmail.com>
2025-02-23 14:18:34 +0530

    update

--- routes/routes.js
+++ routes/routes.js
@@ -6,97 +6,122 @@ const schema = require("../schema/schema")
 routes.get("/data", async (req, res) => {
     try {
         const data = await schema.find()
-    res.status(200).json({
-        success: true,
-        message: "here is the user data",
-        data: data
-    })
+        res.status(200).json({
+            success: true,
+            message: "here is the user data",
+            data: data
+        })
     } catch (error) {
         console.log(error);
     }
-}).get("/user/:id",async (req,res)=>{
-    const {id} = req.params
+}).get("/user/:id", async (req, res) => {
+    const { id } = req.params
     try {
-        const data = await schema.findById({_id:id})
-        if (!data._id) {
+        const data = await schema.findById({ _id: id })
+        if (data == 0) {
             res.status(404).json({
-                success:false,
-                message:"no data found"
+                success: false,
+                message: "no data found"
             })
         }
-    res.status(200).json({
-        success: true,
-        message: "here is the user data",
-        data: data
-    })
-    } catch (error) {
-        console.log({msg:"server errorrrrrrrrrrr"});
-    }
-}).get("/name/:name",async (req,res)=>{
-    const {name} = req.params
-    try {
-        const data = await schema.find({name:name})
-        if (!data.name) {
-            res.status(404).json({
-                success:false,
-                message:"no data found"
-            })
-        }
-    res.status(200).json({
-        success: true,
-        message: "here is the user data",
-        data: data
-    })
-    } catch (error) {
-        console.log({msg:"server error"});
-    }
-}).get("/email/:email",async (req,res)=>{
-    const {email} = req.params
-    try {
-        const data = await schema.find({email:email})
-        if (!data.email) {
-            res.status(404).json({
-                success:false,
-                message:"no data found"
-            })
-        }
-    res.status(200).json({
-        success: true,
-        message: "here is the user data",
-        data: data
-    })
-    } catch (error) {
-        console.log({msg:"server error"});
-    }
-}).get("/Class/:userClass",async (req,res)=>{
-    const {userClass} = req.params
-    try {
-        const data = await schema.find({userClass:userClass})
-        if (!data.userClass) {
-            res.status(404).json({
-                success:false,
-                message:"no data found"
-            })
-        }
-    res.status(200).json({
-        success: true,
-        message: "here is the user data",
-        data: data
-    })
-    } catch (error) {
-        console.log({msg:"server error"});
-    }
-}).delete("/delete/:id",async(req,res)=>{
-    const {id} = req.params
-    try {
-        const data = await schema.findByIdAndDelete({_id:id})
         res.status(200).json({
-            success:true,
-            message:"user delete successfully",
-            data:data
+            success: true,
+            message: "here is the user data",
+            data: data
         })
     } catch (error) {
-        console.log({msg:"server error"});
+        console.log({ msg: "server errorrrrrrrrrrr" });
+    }
+}).get("/name/:name", async (req, res) => {
+    const { name } = req.params
+    try {
+        const data = await schema.find({ name: name })
+        if (data == 0) {
+            res.status(500).json({
+                success: false,
+                message: "no data found"
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: "here is the user data",
+            data: data
+        })
+    } catch (error) {
+        console.log({ msg: "server error" });
+    }
+}).get("/email/:email", async (req, res) => {
+    const { email } = req.params
+    try {
+        const data = await schema.find({ email: email })
+        if (data == 0) {
+            res.status(404).json({
+                success: false,
+                message: "no data found"
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: "here is the user data",
+            data: data
+        })
+    } catch (error) {
+        console.log({ msg: "server error" });
+    }
+}).get("/Class/:userClass", async (req, res) => {
+    const { userClass } = req.params
+    try {
+        const data = await schema.find({ userClass: userClass })
+        if (data == 0) {
+            res.status(404).json({
+                success: false,
+                message: "no data found"
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: "here is the user data",
+            data: data
+        })
+    } catch (error) {
+        console.log({ msg: "server error" });
+    }
+}).delete("/delete/:id", async (req, res) => {
+    const { id } = req.params
+    try {
+        const data = await schema.findByIdAndDelete({ _id: id })
+        res.status(200).json({
+            success: true,
+            message: "user delete successfully",
+            data: data
+        })
+    } catch (error) {
+        console.log({ msg: "server error" });
+    }
+}).patch("/update/:id", async (req, res) => {
+    const { id } = req.params
+    try {
+        const { name, last, age, userClass, email, phone } = req.body;
+        if (!name || !last || !age || !userClass || !email || !phone) {
+            return res.status(500).json({
+                success: false,
+                message: "please fill all the feilds"
+            })
+        }
+        const data = await schema.findByIdAndUpdate({ _id: id }, { $set: req.body })
+        res.status(200).json({
+            success: true,
+            message: "data updated",
+            data: data
+
+        })
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            message: "Server error"
+        });
     }
 })
 
